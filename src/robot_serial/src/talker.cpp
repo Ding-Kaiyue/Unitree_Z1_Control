ecd751c4d9136371b10bea5c6eb412b4bc5252e2
Ding Kaiyue <D570737261@163.com>
2024-06-12 16:19:16 +0800

    添加串口通信发给下位机内容
    Signed-off-by: Ding Kaiyue <D570737261@163.com>

--- src/robot_serial/src/talker.cpp
+++ src/robot_serial/src/talker.cpp
@@ -15,7 +15,7 @@ serial::Serial ros_ser;
 
 int ros_ser_init()
 {
-    ros_ser.setPort("/dev/ttyACM0");
+    ros_ser.setPort("/dev/ttyUSB0");
     ros_ser.setBaudrate(115200);
     serial::Timeout to =serial::Timeout::simpleTimeout(1000);
     ros_ser.setTimeout(to);
@@ -38,6 +38,7 @@ int ros_ser_init()
     {
         return -1;
     }
+    return 0;
 }
 
 class JointStates : public rclcpp :: Node 
@@ -50,17 +51,49 @@ class JointStates : public rclcpp :: Node
         rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscriber_;
         float joint_states_[6] = {0.0f};
         unsigned char buffer[24] = {0};
+
+        void send_position_via_serial(const char* joint_name, double position, double velocity)
+        {
+            uint8_t data[21] = {0};
+            data[0] = 0x55;
+            data[1] = 0xAA;
+            if(strcmp(joint_name, "joint1") == 0) {
+                data[2] = 0x01;
+            }
+            else if(strcmp(joint_name, "joint2") == 0) {
+                data[2] = 0x02;
+            }
+            else if(strcmp(joint_name, "joint3") == 0) {
+                data[2] = 0x03;
+            }
+            else if(strcmp(joint_name, "joint4") == 0) {
+                data[2] = 0x04;
+            }
+            else if(strcmp(joint_name, "joint5") == 0) {
+                data[2] = 0x05;
+            }
+            else if(strcmp(joint_name, "joint6") == 0) {
+                data[2] = 0x06;
+            }
+            else {
+                data[2] = 0x07;
+            }
+            memcpy(&data[3], &position, sizeof(double));
+            memcpy(&data[11], &velocity, sizeof(double));
+            data[19] = 0xEB;
+            data[20] = 0xAA;
+            // std::string data = std::to_string(position);
+            ros_ser.write(data, sizeof(data));
+        }
+        
         void joint_states_callback(const sensor_msgs::msg::JointState::SharedPtr msg) {    
             for(size_t i = 0; i < msg->name.size(); ++i) {
-                RCLCPP_INFO(this->get_logger(), "joint %s position %f", msg->name[i].c_str(), msg->position[i]);
-                send_position_via_serial(msg->position[i]);
+                RCLCPP_INFO(this->get_logger(), "joint %s position %f velocity %f", msg->name[i].c_str(), msg->position[i], msg->velocity[i]);
+                send_position_via_serial(msg->name[i].c_str(), msg->position[i], msg->velocity[i]);
+                std::this_thread::sleep_for(std::chrono::milliseconds(5)); 
             }
         }
-        void send_position_via_serial(double position)
-        {
-            std::string data = std::to_string(position);
-            ros_ser.write(data);
-        }
+        
 };
 
 int main(int argc, char const *argv[])
